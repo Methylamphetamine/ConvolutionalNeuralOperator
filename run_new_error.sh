b1d087ba447bccde9d6f637edd4a54223666265e
methylamphetamine <cngryphus@gmail.com>
2023-11-27 13:21:20 -0500

    losses

--- run_new_error.sh
+++
@@ -1,9 +0,0 @@
-python TrainCNO_rel_l2.py shear_layer
-python TrainCNO_rel_l2.py poisson
-python TrainCNO_rel_l2.py wave_0_5
-python TrainCNO_rel_l2.py allen
-python TrainCNO_rel_l2.py cont_tran
-python TrainCNO_rel_l2.py disc_tran
-python TrainCNO_rel_l2.py airfoil
-python TrainCNO_rel_l2.py darcy
-python TrainCNO_rel_l2.py ns
